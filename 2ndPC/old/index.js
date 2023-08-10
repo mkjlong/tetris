74306a0a7ff2cb0cd2b6ad136ba136e6cd0fd554
mkjlong <mkjlong@gmail.com>
2023-08-09 20:59:07 -0400

    fix bug

---
+++ 2ndPC/old/index.js
@@ -0,0 +1,376 @@
+const data = {
+    "TIJL":{
+        category:1,
+        display_setup:"v115@Rhg0Hegli0zhilJeAgH",
+        description:"wip dont judge this"
+    },
+    "TIJO":{
+        category:1,
+        display_setup:"v115@Ghwhh0Fewwwhg0RpDexwwhg0RpEewwwhJeAgH",
+        description:"wip dont judge this"
+    },
+    "TIJS":{
+        category:1,
+        display_setup:"v115@Lhg0GeR4g0FeR4h0AezhJeAgH",
+        description:"wip dont judge this"
+    },
+    "TIJZ":{
+        category:1,
+        display_setup:"v115@GhwhBtFewwwhg0BtDexwwhi0EewwwhJeAgH",
+        description:"wip dont judge this"
+    },
+    "TILO":{
+        category:1,
+        display_setup:"v115@9gwhIewhwwFehlwhxwDeRpglwhwwEeRpglJeAgH",
+        description:"wip dont judge this"
+    },
+    "TILS":{
+        category:1,
+        display_setup:"v115@9gwhIewhwwFeR4whxwDeR4glwhwwEeilJeAgH",
+        description:"wip dont judge this"
+    },
+    "TILZ":{
+        category:1,
+        display_setup:"v115@MhglIeglBtBezhAehlBtKeAgH",
+        description:"wip dont judge this"
+    },
+    "TIOS":{
+        category:1,
+        display_setup:"v115@GhwhIewhCeR4wwCewhBeR4ywBewhJeAgH",
+        description:"wip dont judge this"
+    },
+    "TIOZ":{
+        category:1,
+        display_setup:"v115@9gwhIewhIewhCewwBtCewhBeywBtLeAgH",
+        description:"wip dont judge this"
+    },
+    "TISZ":{
+        category:1,
+        display_setup:"v115@JhBtHewwBtR4DeywR4zhJeAgH",
+        description:"wip dont judge this"
+    },
+    "TJSZ":{
+        category:1,
+        display_setup:"v115@9gQ4IeR4CewwDeg0Q4BtxwDei0BtwwNeAgH",
+        description:"wip dont judge this"
+    },
+    "TJOS":{
+        category:1,
+        display_setup:"v115@9gwwIexwFeRpwwR4Deg0RpR4Eei0JeAgH",
+        description:"wip dont judge this"
+    },
+    "TJOZ":{
+        category:1,
+        display_setup:"v115@Ghwwh0Fexwg0RpDeBtwwg0RpEeBtJeAgH",
+        description:"wip dont judge this"
+    },
+    "TLSZ":{
+        category:1,
+        display_setup:"v115@GhAtDewwCeBtDexwR4AtglDewwR4ilJeAgH",
+        description:"wip dont judge this"
+    },
+    "TLOS":{
+        category:1,
+        display_setup:"v115@9gwwIexwFehlwwR4DeRpglR4EeRpglJeAgH",
+        description:"wip dont judge this"
+    },
+    "TLOZ":{
+        category:1,
+        display_setup:"v115@GhwwRpFexwRpglDeBtwwilEeBtJeAgH",
+        description:"wip dont judge this"
+    },
+    "TOSZ":{
+        category:1,
+        display_setup:"v115@KhAtFeRpBtR4DeRpAtR4OeAgH",
+        description:"wip dont judge this"
+    },
+    "IJLO":{
+        category:2,
+        display_setup:"v115@Rhg0DeglBeRpi0ilBeRpJeAgH",
+        description:"wip dont judge this"
+    },
+    "IJLS":{
+        category:2,
+        display_setup:"v115@GhwhIewhg0Geglwhi0CeilwhJeAgH",
+        description:"wip dont judge this"
+    },
+    "IJLZ":{
+        category:2,
+        display_setup:"v115@9gwhIewhIewhg0Geglwhi0CeilJeAgH",
+        description:"wip dont judge this"
+    },
+    "IOSZ":{
+        category:2,
+        display_setup:"v115@KhAtFeRpBtR4DeRpAtR4OeAgH",
+        description:"wip dont judge this"
+    },
+    "JLOS":{
+        category:2,
+        display_setup:"v115@9gQ4IeR4Heg0Q4CeRpBegli0BeRpilJeAgH",
+        description:"wip dont judge this"
+    },
+    "JLOZ":{
+        category:2,
+        display_setup:"v115@GhAtHeBtg0BeRpCeAtgli0RpBeilJeAgH",
+        description:"wip dont judge this"
+    },
+    "JLSZ":{
+        category:2,
+        display_setup:"v115@9gQ4IeR4Heg0Q4BtEegli0BtBeilJeAgH",
+        description:"wip dont judge this"
+    },
+    "IJOS":{
+        category:3,
+        display_setup:"v115@OhR4GeR4RpDezhRpJeAgH",
+        description:"wip dont judge this"
+    },
+    "IJOZ":{
+        category:3,
+        display_setup:"v115@GhwhIewhRpFeglwhRpDeilwhJeAgH",
+        description:"wip dont judge this"
+    },
+    "IJSZ":{
+        category:3,
+        display_setup:"v115@9gQ4HewhR4Gewhg0Q4BtEewhi0BtDewhJeAgH",
+        description:"wip dont judge this"
+    },
+    "ILOS":{
+        category:3,
+        display_setup:"v115@9gwhIewhIewhg0FeRpwhi0DeRpJeAgH",
+        description:"wip dont judge this"
+    },
+    "ILOZ":{
+        category:3,
+        display_setup:"v115@IhBtGeRpBtFeRpzhNeAgH",
+        description:"wip dont judge this"
+    },
+    "ILSZ":{
+        category:3,
+        display_setup:"v115@9gwhHeAtwhGeBtwhEeR4AtglwhDeR4ilJeAgH",
+        description:"wip dont judge this"
+    },
+    "JOSZ":{
+        category:3,
+        display_setup:"v115@9gQ4IeR4Heg0Q4BtDeRpi0BtCeRpJeAgH",
+        description:"wip dont judge this"
+    },
+    "LOSZ":{
+        category:3,
+        display_setup:"v115@GhAtHeBtRpDeR4AtglRpCeR4ilJeAgH",
+        description:"wip dont judge this"
+    },
+}
+
+
+
+
+keyMap = {}
+
+
+
+var copyfumen = "v115@vhAAgH"
+var copyimage = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAANwAAABuCAYAAABMWH3/AAAAAXNSR0IArs4c6QAAAoVJREFUeF7t00ENAAAIAzHm3zQu7lUMLGm4nSNAIBNYtmSIAIETnCcgEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUAcH5AQKhgOBCbFMEBOcHCIQCgguxTREQnB8gEAoILsQ2RUBwfoBAKCC4ENsUgQeRNABv+HSJhwAAAABJRU5ErkJggg=='
+
+
+const qbsearch = $("#qbsearch > input")
+
+
+
+
+qbsearch.on("keydown",function(e){
+    const key = e.key;
+    const control = e.ctrlKey;
+    if (control || e.key.length > 1) {
+        return;
+    }
+    else if (!(/[TIJLOSZ]/gi.test(key))) {
+        e.preventDefault();
+    }
+
+    newWord = (qbsearch.val() + e.key).toUpperCase()
+    if(newWord.length==5){
+        return
+    }
+
+    //prevent 3 piece duplicates
+    for(letter of newWord){
+        if(newWord.match(new RegExp(letter,"g")).length>=2){
+            e.preventDefault()
+        }
+    }
+})
+
+
+qbsearch.on('input',selection_screen)
+
+$(".setupgrid").on("wheel", function (evt){
+    children_width = document.querySelector("#group2 > div.setupgrid > div:nth-child(1)").offsetWidth
+    
+    if(evt.originalEvent.deltaY>0){
+        new_scroll = this.scrollLeft + children_width + 0.03 * document.body.offsetWidth
+    }else{
+        new_scroll = this.scrollLeft - children_width - 0.03 * document.body.offsetWidth
+    }
+    new_scroll = Math.max(0,new_scroll)
+    new_scroll = Math.min(this.scrollWidth - this.offsetWidth, new_scroll)
+    this.scrollLeft = new_scroll
+    scroll_percent = new_scroll / (this.scrollWidth - this.offsetWidth)
+    $(this.previousElementSibling).children()[0].style.width = `${scroll_percent * 100}%`
+    
+});
+
+
+
+
+function selection_screen(){
+    queue = qbsearch.val().toUpperCase()
+    
+    $("#search").removeClass("hidden")
+    $("#setupfocus").addClass("hidden")
+    $(`.setupgrid`).empty()
+    $("#search").children().each(function(){
+        $(this).addClass("hidden")
+    })
+
+    loop1: for(second in data){
+        for(letter of queue){
+            if(!second.includes(letter)){
+                continue loop1;
+            }
+        }
+
+        
+
+        div = $(document.createElement('div'))
+
+        name_tag = $(document.createElement('span'))
+
+        name_tag.html(second)
+
+        img = document.createElement("img")
+        img.src = getDataURL(data[second].display_setup)
+        $(img).attr("fumen", data[second].display_setup)
+        $(`#group${data[second].category}`).removeClass("hidden")
+        $(`#group${data[second].category}>.setupgrid`).append(div)
+        div.append(name_tag)
+        div.append(img)
+    }
+    for(element of document.querySelectorAll(".setupgrid > div")){
+        element.addEventListener("click",function(e){
+            focus($($(this).children()[0]).html())
+        },{passive:true})
+        element.addEventListener("contextmenu",function(e){
+            thing = $($(this).children()[0]).html()
+            $("#rightclick").removeClass("hidden")
+            $("#rightclick").css("top", e.clientY + 'px')
+            $("#rightclick").css("left", e.clientX + 'px')
+            copyimage = $(this).children()[1].src
+            copyfumen = $($(this).children()[1]).attr("fumen")
+            e.preventDefault();
+        })
+    }
+    $(".setupgrid").each(function(){
+        scroll_percent = this.scrollLeft / (this.scrollWidth - this.offsetWidth)
+        if(this.scrollWidth == this.offsetWidth){
+            scroll_percent = 1
+        }
+        $(this.previousElementSibling).children()[0].style.width = `${scroll_percent * 100}%`
+    })
+}
+
+
+
+
+function focus(second){
+    const {display_setup,description} = data[second]
+    $("#search").addClass("hidden")
+    $("#setupfocus").removeClass("hidden")
+    $("#title").html("")
+
+    $("#title").html(second)
+    $("#subtitle").html(description)
+
+    $("#setupfocus img").attr("src",getDataURL(display_setup))
+    console.log($("#title")[0].getBoundingClientRect().y);
+    
+}
+
+
+selection_screen()
+
+
+
+document.addEventListener("keyup",function(e){
+    if(e.key=="Escape"){
+        if($("#search").hasClass("hidden")){
+            selection_screen()
+        }
+    }
+    
+});
+window.addEventListener('click',function(e){
+    if(e.target.offsetParent != $("#rightclick")[0]){
+        $("#rightclick").addClass('hidden')
+        $("#rightclick").css({"left":"","top":""})
+    }
+    e.preventDefault()
+})
+window.oncontextmenu=function(e){
+    e.preventDefault()
+}
+
+
+
+document.onkeydown = document.onkeyup = function(e){
+    if(e.repeat)return;
+    keyMap[e.key]=e.type=='keyup'
+}
+
+
+//focus("TJOS")
+
+
+$("img").on("dragstart",function(){
+    return false;
+})
+
+
+
+$(".title").on("click",function(e){
+    setupgrid = $(this).parent().children()[1]
+    scroll_width = setupgrid.scrollWidth - setupgrid.offsetWidth
+    percent = e.clientX / document.body.offsetWidth;
+    $(this).parent().children()[1].scrollLeft = percent * scroll_width
+    $(this).children()[0].style.width = `${percent * 100}%`
+})
+
+
+$("#rightclick > span").on("click",function(e){
+    if($(this).html()=="Copy Fumen"){
+        console.log(copyfumen);
+        navigator.clipboard.writeText(copyfumen)
+    }else if($(this).html()=="Copy Image"){
+        sliceSize = 512
+        dataurl = copyimage.split(',').slice(-1)[0]
+        let byteCharacters = atob(dataurl)
+        let byteArrays = []
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            let slice = byteCharacters.slice(offset, offset + sliceSize)
+            let byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i)
+            }
+            var byteArray = new Uint8Array(byteNumbers)
+            byteArrays.push(byteArray)
+        }
+        blob = new Blob(byteArrays, {type: 'image/png'})
+        navigator.clipboard.write([new ClipboardItem({'image/png': blob})])
+    }
+    
+    $("#rightclick").addClass('hidden')
+    $("#rightclick").css({"left":"","top":""})
+})
+
+
+
+
